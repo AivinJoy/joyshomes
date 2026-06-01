72eb8c85442951dcd4e04c172f0c6d734f8fca63
AivinJoy <aivincjoy@gmail.com>
2026-06-01 22:34:04 +0530

    gallery section completetd.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
    import Hero from "$lib/components/Hero.svelte";
    import Carousel from "$lib/components/Carousel.svelte";
    import Achievements from "$lib/components/Achievements.svelte";
+   import Gallery from "$lib/components/Gallery.svelte";
 </script>
 
 <div class="h-[300vh] w-full relative z-0">
@@ -14,4 +15,8 @@
 
 <div class="relative z-20">
     <Achievements />
+</div>
+
+<div class="relative z-30 shadow-2xl">
+    <Gallery/>
 </div>
